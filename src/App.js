b4cad5d3dda8e86a23e29c9446e6da1ff78624f7
Iva Bozic <ivabozic23@gmail.com>
2021-01-07 16:11:24 +0000

    added input field

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-// import DateTimePicker from "./components/DateTimePicker";
+import DateTimePicker from "./components/DateTimePicker";
 import CountdownVisualizer from "./components/CountdownVisualizer";
 import "./App.css";
 
 export default class App extends Component {
   state = {
-    endTime: "2020-11-19T00:01",
+    endTime: "",
   };
 
   getEndTime = (endTime) => {
@@ -19,10 +19,10 @@ export default class App extends Component {
     const { endTime } = this.state;
     return (
       <div className='app'>
-        <img src='/img/CP2077.png' alt='Cyberpunk 2077 logo' />
-        {/* <div className='input-container'>
+        {/* <img src='/img/CP2077.png' alt='Cyberpunk 2077 logo' /> */}
+        <div className='input-container'>
           <DateTimePicker getEndTime={this.getEndTime} />
-        </div> */}
+        </div>
         <div className='countdown-container'>
           {endTime && <CountdownVisualizer endTime={endTime} />}
         </div>
